bcdc3cda0eccafd2df3813e645e687569bea9334
Chris Hall <chall@wapacklabs.com>
2014-07-25 15:48:44 -0400

    removed port 8080

--- simple_search.py
+++ simple_search.py
@@ -9,7 +9,7 @@ api_key = 'my API key'
 
 def query_threat_recon(indicator, api_key):
     params = urllib.urlencode({'api_key': api_key, 'indicator': indicator})
-    f = urllib2.urlopen("https://api.threatrecon.co:8080/api/v1/search", params)
+    f = urllib2.urlopen("https://api.threatrecon.co/api/v1/search", params)
     data = json.load(f)
     results = data["Results"]
     print json.dumps(data, indent=4, sort_keys=False)    
