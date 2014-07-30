3331631042d823e444760ef47a9e3d9e8cbf8f9b
Chris Hall <chall@wapacklabs.com>
2014-07-29 20:23:00 -0400

    added host IP lookup functionality per request

--- simple_search.py
+++ simple_search.py
@@ -2,10 +2,16 @@ import urllib
 import urllib2
 from urllib2 import urlopen, quote
 import json
+import re,sys
+import socket
+
 
 search = 'serval.essanavy.com'
 
 api_key = 'my API key'
+
+def search_is_domain(strg, search=re.compile(r"^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$", re.I).search):
+    return bool(search(strg))
 
 def query_threat_recon(indicator, api_key):
     params = urllib.urlencode({'api_key': api_key, 'indicator': indicator})
@@ -16,3 +22,15 @@ def query_threat_recon(indicator, api_key):
     return results
 
 results = query_threat_recon(search, api_key)
+
+#check host IP if no results
+if results == None:
+    if search_is_domain(search):
+        try: # tries to get IP from domain
+            iplookup = socket.gethostbyname(search)
+            print '\n'
+            print '*****No results found for this domain... checking host IP:'+iplookup
+            print '\n'
+            results = query_threat_recon(iplookup, api_key)
+        except:
+            iplookup = 'no joy'
