66aa9045e46b3cb79d80656d7b0df3a47e16c846
Seth Bromberger <seth@ncisecurity.com>
2014-08-10 23:28:27 -0700

    More cleanup - created TRIndicator objects and both raw and cooked versions of query_threat_recon for examples/production.
    Removed code for checking whether a string is a valid domain -
    this is fairly useless given the new TLDs and unicode rules.

--- simple_search.py
+++ simple_search.py
@@ -3,7 +3,7 @@ import socket
 import json
 import argparse
 from api import get_api_key, APIError
-from query import search_is_domain, query_threat_recon
+from query import raw_query_threat_recon
 
 search_default = 'serval.essanavy.com'
 api_key_default = get_api_key() or 'my API key'
@@ -31,7 +31,7 @@ if __name__ == "__main__":
     print "***** Searching %s" % search
 
     try:
-        results = query_threat_recon(search, api_key)
+        results = raw_query_threat_recon(search, api_key)
     except APIError as e:
         print "***** API Error: %s" % e
         exit(1)
@@ -42,29 +42,25 @@ if __name__ == "__main__":
     else:
         # No results - check host IP
         print "***** No results found for search term %s..." % search
-        if search_is_domain(search):
-            print "***** %s is a valid domain." % search
-            # This is a valid domain name: try reversing DNS
+        print "***** searching %s as a domain." % search
+        # Try reversing DNS
+        try:
+            iplookup = socket.gethostbyname(search)
+            print "***** Checking host IP: %s\n" % iplookup
             try:
-                iplookup = socket.gethostbyname(search)
-                print "***** Checking host IP: %s\n" % iplookup
-                try:
-                    results = query_threat_recon(iplookup, api_key)
-                except APIError as e:
-                    print "***** API Error: %s" % e
-                    exit(1)
-                if results:
-                    # DNS lookup successful and we have results
-                    print "%s" % json.dumps(results, indent=4, sort_keys=False)
-                else:
-                    # DNS lookup successful and there were no results.
-                    print "***** No results found for IP %s." % iplookup
-            except socket.gaierror as e:
-                # DNS lookup unsuccessful.
-                print "***** Error in IP lookup: %s." % e
-            except Exception as e:
-                raise
-        else:
-            # This is not a valid domain name. Abort.
-            print "***** %s is not a valid domain. Search terminated." % search
+                results = raw_query_threat_recon(iplookup, api_key)
+            except APIError as e:
+                print "***** API Error: %s" % e
+                exit(1)
+            if results:
+                # DNS lookup successful and we have results
+                print "%s" % json.dumps(results, indent=4, sort_keys=False)
+            else:
+                # DNS lookup successful and there were no results.
+                print "***** No results found for IP %s." % iplookup
+        except socket.gaierror as e:
+            # DNS lookup unsuccessful.
+            print "***** Error in IP lookup: %s." % e
+        except Exception as e:
+            raise
     exit(0)
