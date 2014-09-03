0b5c0720b8fb7262498b34210ff99b1d07cd919c
Chris Hall <chall@wapacklabs.com>
2014-09-03 10:29:27 -0400

    added new search fields
    - attribution, comment, reference

--- threatrecon/query.py
+++ threatrecon/query.py
@@ -22,6 +22,9 @@ from dateutil import parser
 from api import API_FIELDS, APIError
 
 
+###########RAW QUERY############################################
+################################################################
+
 def raw_query_threat_recon(indicator, api_key):
     """
     Uses an indicator and the api key to query the threat recon
@@ -69,6 +72,165 @@ def query_threat_recon(indicator, api_key):
 
         results.append(i)
     return results
+
+
+###########ATTRIBUTION QUERY####################################
+################################################################
+
+def raw_query_threat_recon_attribution(indicator, api_key):
+    """
+    Uses an indicator and the api key to query the threat recon
+    database. Returns a list of python dicts corresponding to
+    the JSON output from the server (results only). If no results,
+    returns an empty list.
+
+    Will throw APIError exception if the ResponseCode < 0.
+
+    """
+    # NOTE: results will have mixed-case keys. In general, these
+    # should not be used directly - the TRIndicator objects use
+    # lower-case attributes (and generate dicts with lower-case keys).
+
+    params = urllib.urlencode({'api_key': api_key, 'attribution': indicator})
+    urllib2.install_opener(urllib2.build_opener(HTTPSHandlerV3()))
+
+    f = urllib2.urlopen("https://api.threatrecon.co/api/v1/search/attribution", params)
+    data = json.load(f)
+    response = data.get("ResponseCode", -99)
+    if response < 0:
+        raise APIError(response)
+
+    results = data.get("Results", [])
+    lowerresults = []
+    if results:         # can be None, so make sure to check
+        for r in results:
+            lowerresults.append({k.lower(): v for k, v in r.items()})
+
+    return lowerresults
+
+
+def query_threat_recon_attribution(indicator, api_key):
+    """
+    Calls _raw_query_threat_recon and returns a list of TRIndicator
+    objects with appropriate attributes set.
+    """
+    results = []
+    for r in raw_query_threat_recon_attribution(indicator, api_key):
+        i = TRIndicator(**r)
+        i._query_root = (
+            i.rootnode == '' and
+            i.indicator == indicator
+        )
+
+        results.append(i)
+    return results
+
+
+###########REFERENCE QUERY####################################
+################################################################
+
+def raw_query_threat_recon_reference(indicator, api_key):
+    """
+    Uses an indicator and the api key to query the threat recon
+    database. Returns a list of python dicts corresponding to
+    the JSON output from the server (results only). If no results,
+    returns an empty list.
+
+    Will throw APIError exception if the ResponseCode < 0.
+
+    """
+    # NOTE: results will have mixed-case keys. In general, these
+    # should not be used directly - the TRIndicator objects use
+    # lower-case attributes (and generate dicts with lower-case keys).
+
+    params = urllib.urlencode({'api_key': api_key, 'reference': indicator})
+    urllib2.install_opener(urllib2.build_opener(HTTPSHandlerV3()))
+
+    f = urllib2.urlopen("https://api.threatrecon.co/api/v1/search/reference", params)
+    data = json.load(f)
+    response = data.get("ResponseCode", -99)
+    if response < 0:
+        raise APIError(response)
+
+    results = data.get("Results", [])
+    lowerresults = []
+    if results:         # can be None, so make sure to check
+        for r in results:
+            lowerresults.append({k.lower(): v for k, v in r.items()})
+
+    return lowerresults
+
+
+def query_threat_recon_reference(indicator, api_key):
+    """
+    Calls _raw_query_threat_recon and returns a list of TRIndicator
+    objects with appropriate attributes set.
+    """
+    results = []
+    for r in raw_query_threat_recon_reference(indicator, api_key):
+        i = TRIndicator(**r)
+        i._query_root = (
+            i.rootnode == '' and
+            i.indicator == indicator
+        )
+
+        results.append(i)
+    return results
+
+
+###########COMMENT QUERY########################################
+################################################################
+
+def raw_query_threat_recon_comment(indicator, api_key):
+    """
+    Uses an indicator and the api key to query the threat recon
+    database. Returns a list of python dicts corresponding to
+    the JSON output from the server (results only). If no results,
+    returns an empty list.
+
+    Will throw APIError exception if the ResponseCode < 0.
+
+    """
+    # NOTE: results will have mixed-case keys. In general, these
+    # should not be used directly - the TRIndicator objects use
+    # lower-case attributes (and generate dicts with lower-case keys).
+
+    params = urllib.urlencode({'api_key': api_key, 'comment': indicator})
+    urllib2.install_opener(urllib2.build_opener(HTTPSHandlerV3()))
+
+    f = urllib2.urlopen("https://api.threatrecon.co/api/v1/search/comment", params)
+    data = json.load(f)
+    response = data.get("ResponseCode", -99)
+    if response < 0:
+        raise APIError(response)
+
+    results = data.get("Results", [])
+    lowerresults = []
+    if results:         # can be None, so make sure to check
+        for r in results:
+            lowerresults.append({k.lower(): v for k, v in r.items()})
+
+    return lowerresults
+
+
+def query_threat_recon_comment(indicator, api_key):
+    """
+    Calls _raw_query_threat_recon and returns a list of TRIndicator
+    objects with appropriate attributes set.
+    """
+    results = []
+    for r in raw_query_threat_recon_comment(indicator, api_key):
+        i = TRIndicator(**r)
+        i._query_root = (
+            i.rootnode == '' and
+            i.indicator == indicator
+        )
+
+        results.append(i)
+    return results
+
+
+################################################################
 
 
 class TRIndicator(object):
