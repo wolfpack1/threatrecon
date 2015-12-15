bc012b36086ae2b6eac90565acb2ea81062ccce0
Chris Hall <chall@wapacklabs.com>
2015-12-15 07:42:42 -0500

    Update threatrecon_stix_rss.py

--- examples/threatrecon_stix_rss.py
+++ examples/threatrecon_stix_rss.py
@@ -1,3 +1,19 @@
+"""
+Copyright (C) 2015 by Wapack Labs Corporation
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+#This is a POC program checks the Threat Recon RSS feed for new content. If new content is found, a Threat Recon query is triggered and a STIX package for the content is downloaded
+
+
 import sys
 import os
 import re
